b6b2fb71921b0ffe3446663359cd154f8dda7fd0
dogancankilment <dogancankilment@gmail.com>
2014-07-11 13:17:42 +0300

    PEP8 configure

--- unshredder.py
+++ unshredder.py
@@ -31,14 +31,13 @@ def unshred(image, shred_width):
     Unshred yapilmis olan resme yerlestirmelidir.
 
     Argumanlar:
-
     image : resim
     shred_width : shred genisligi
     return :
     Buradan geri donecek deger (return yapacak sey)
     Unshred yapilmis olan resmi olusturacak shred listesini tutacak
-
     """
+
     def unshred_islemi(yerlestirme, shreds_left, unshred_order):
 
         """
@@ -56,17 +55,20 @@ def unshred(image, shred_width):
         Bu parca yerlestirildikten sonra bir sonraki parcayi bulmak icin
         Hesaplama sonucunda en dusuk degere sahip olan parcayi yerlestirebilmek icin
         en son gecerli olan unshredded resmin solundaki parca ile karsilastirildiktan sonra
-        unshred order listesinin basina eklenmeli. Bu sayede liste basina ekledigimiz yeni parca
-        gecerli olan unshredded resmimizin sag parcasi olacaktir.
+        unshred order listesinin basina eklenmeli. Bu sayede liste basina
+        ekledigimiz yeni parca gecerli olan unshredded resmimizin sag parcasi olacaktir.
 
-        Eger baslangicta unshred order listemiz bos ise yerlestirmeye baslarken en dusuk degerli olan
-        parcayi alip, cift tarafli bakarak yerlestirmeye devam etmeliyiz.
+        Eger baslangicta unshred order listemiz bos ise yerlestirmeye baslarken
+        en dusuk degerli olan parcayi alip, cift tarafli bakarak yerlestirmeye devam etmeliyiz.
         Zaten cift tarafli bakacagimiz icin resim en sonunda ortaya cikacaktir.
-
         """
+
         if shreds_left == 0:
+
             return unshred_order
+
         else:
+
             if not unshred_order: #liste ilk adimda bos ise
 
                 best = yerlestirme[0]
@@ -77,10 +79,12 @@ def unshred(image, shred_width):
 
                 #en iyi liste basina eklenebilecek deger
                 best_prepend = filter(lambda x: x[1] == unshred_order[0], yerlestirme)
+
                 #en iyi tercih
                 best_append = filter(lambda x: x[0] == unshred_order[-1], yerlestirme)
 
-                if best_prepend and best_append and best_prepend[0][2] < best_append[0][2] or best_prepend:
+                if best_prepend and best_append and \
+                                best_prepend[0][2] < best_append[0][2] or best_prepend:
 
                     unshred_order_next = [best_prepend[0][0]] + unshred_order
 
@@ -90,7 +94,7 @@ def unshred(image, shred_width):
 
                 else:
 
-                    raise Exception("can't happen")
+                    raise Exception("Yapilamadi")
 
                 shreds_left_next = shreds_left - 1
 
@@ -98,24 +102,34 @@ def unshred(image, shred_width):
 
                 Simdi elimizde olanlar
                 unshred_order_next[0] : sol parca ve sol parcayla devam eden
-                unshred_order_next[-1] : unshred_order_next'in son elemani - sag parca gibi
+                unshred_order_next[-1] : unshred_order_next'in
+                                        son elemani - sag parca gibi
 
                 """
 
-            yerlestirme_next = filter(lambda x: x[0] != unshred_order_next[0] and x[1] != unshred_order_next[-1], yerlestirme)
+            yerlestirme_next = filter(lambda x: x[0] != unshred_order_next[0] and
+                                                x[1] != unshred_order_next[-1], yerlestirme)
 
             return unshred_islemi(yerlestirme_next, shreds_left_next, unshred_order_next)
 
     shreds = image.size[0] / shred_width
-    #cmp karsilastirma islemi yapmamizi sagliyor
-    # Eger geri donen deger negatif ise if x < y, sifir ise if x == y ve tam anlamiyla pozitif ise if x > y.
-    yerlestirme = sorted([(sol, sag, pixel_islemleri(image, shred_width, sol, sag)) for sol in xrange(shreds) for sag in xrange(shreds) if sol != sag], cmp=lambda x, y: x[2] - y[2])
+
+    # cmp karsilastirma islemi yapmamizi sagliyor
+    # Eger geri donen deger negatif ise if x < y, sifir ise if x == y ve
+    # tam anlamiyla pozitif ise if x > y.
+    yerlestirme = sorted([(sol, sag, pixel_islemleri(image, shred_width, sol, sag))
+                          for sol in xrange(shreds) for sag in xrange(shreds)
+                          if sol != sag], cmp=lambda x, y: x[2] - y[2])
 
     return unshred_islemi(yerlestirme, shreds, [])
 
 
 if __name__ == "__main__":
-    image = PIL.Image.open(sys.argv[1]) # kullanici girdi olarak yazdigi resmi isleme tabi tutacagiz
+
+    # kullanicinin girdi olarak yazdigi resmi
+    # isleme tabi tutacagiz
+    image = PIL.Image.open(sys.argv[1])
+
     shred_width = 32 #her bir parcanin genisligi
     unshred_order = unshred(image.convert("L"), shred_width) #monochrome gri tonlama
 
@@ -129,6 +143,8 @@ if __name__ == "__main__":
         x1, y1 = shred_width * shred, 0
         x2, y2 = x1 + shred_width, image.size[1]
         unshredded.paste(image.crop((x1, y1, x2, y2)), (i * shred_width, 0))
+        # step by step gormemizi sagliyor
+        # unshredded.save(str(i)+"unshredded.png", "PNG")
 
     unshredded.save("unshredded.png", "PNG")
     #unshredded.save("unshredded.jpg")
